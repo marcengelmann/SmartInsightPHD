2b2fba4d790456af7f0ba4d4e854f4007da9b42b
rebeccaForster <rebecca_forster@web.de>
2016-01-12 23:58:42 +0100

    HInzufügen bei der Klausurliste, dass ein aktueller KLausureinsichttermin grün hinterlegt ist

--- app/src/main/java/de/tum/mw/ftm/praktikum/smartinsightphd/CalendarListAdapter.java
+++ app/src/main/java/de/tum/mw/ftm/praktikum/smartinsightphd/CalendarListAdapter.java
@@ -1,11 +1,16 @@
 package de.tum.mw.ftm.praktikum.smartinsightphd;
 
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,10 +32,30 @@ public class CalendarListAdapter extends RecyclerView.Adapter<CalendarListAdapte
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        String date = mValues.get(position).date;
         viewHolder.mItem = mValues.get(position);
+        SimpleDateFormat curFormater = new SimpleDateFormat("dd.MM.yyyy");
+        Date examDate = null;
+        Date today;
+        try {
+            examDate = curFormater.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date currentDay = new Date();
+        if(examDate != null){
+            if(examDate.getYear() == currentDay.getYear()
+                    && examDate.getMonth() == currentDay.getMonth()
+                    && examDate.getDay() == currentDay.getDay()){
+                viewHolder.card.setCardBackgroundColor(Color.GREEN);
+            } else{
+                viewHolder.card.setCardBackgroundColor(Color.WHITE);
+
+            }
+        }
         viewHolder.name.setText(mValues.get(position).name);
         viewHolder.room.setText(mValues.get(position).room);
-        viewHolder.date.setText(mValues.get(position).date);
+        viewHolder.date.setText(date);
         viewHolder.numbOfReg.setText(mValues.get(position).numbOfRegistration);
         viewHolder.responsiblePerson.setText(mValues.get(position).responsiblePerson);
         viewHolder.mean.setText(mValues.get(position).mean);
@@ -52,6 +77,7 @@ public class CalendarListAdapter extends RecyclerView.Adapter<CalendarListAdapte
         public TextView numbOfReg;
         public TextView responsiblePerson;
         public TextView mean;
+        private CardView card;
 
         public ViewHolder(View view) {
             super(view);
@@ -62,6 +88,7 @@ public class CalendarListAdapter extends RecyclerView.Adapter<CalendarListAdapte
             numbOfReg = (TextView) view.findViewById(R.id.calendar_numbOfReg);
             responsiblePerson = (TextView) view.findViewById(R.id.calendar_responsiblePerson);
             mean = (TextView) view.findViewById(R.id.calendar_mean);
+            card = (CardView) view.findViewById(R.id.card_view_calendar_list);
 
         }
 
